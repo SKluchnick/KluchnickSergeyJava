a9afe66aba59e678976d0b6bea87e69e6bcaf34f
SergeyGitAccount <l_w_d@i.ua>
2022-04-14 21:15:39 +0300

    Initial commit

---
+++ src/allTasks/Train/TaskThree/Main.java
@@ -0,0 +1,8 @@
+package allTasks.Train.TaskThree;
+
+public class Main {
+    public static void main(String[] args) {
+        ImplementArray implementArray = new ImplementArray();
+        implementArray.getArray();
+    }
+}
